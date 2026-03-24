198ba4fc7499c6fabec31df8a331645262ec5abb
Hanumanthareddy <reddyreddy884@gmail.com>
2026-03-24 23:52:54 +0530

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [LoginComponent,DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
